8f077a81f4a208d92b2ec7787f8e99d97524dbc9
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-01-30 07:57:55 -0700

    Update script.py

--- script.py
+++ script.py
@@ -11,4 +11,4 @@ ydl_opts = {
     }],
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
+    ydl.download(['https://music.youtube.com/playlist?list=PLTZLpQSU2D776UBx2q44uBvzGvrTXtZu2'])
