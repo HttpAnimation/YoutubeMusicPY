4fbb6a0719108d31b34405c474f0540a8a7ba153
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2024-01-30 07:59:18 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
-from __future__ import unicode_literals
 import youtube_dl
 
+# Replace the playlist URL with your desired playlist URL
+playlist_url = "https://music.youtube.com/playlist?list=PLTZLpQSU2D776UBx2q44uBvzGvrTXtZu2"
 
+# Set options for youtube_dl
 ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
@@ -9,6 +11,9 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
+    'outtmpl': '%(title)s.%(ext)s',
 }
+
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://music.youtube.com/playlist?list=PLTZLpQSU2D776UBx2q44uBvzGvrTXtZu2'])
+    # Download the playlist
+    ydl.download([playlist_url])
